e314848a4ccb5263575233b674aa06a65a9bc852
awen0317 <awen0317.163.com>
2019-09-26 08:14:54 +0000

    显示个人资料

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -7,10 +7,10 @@
                 <img class="card-img-top" src="https://iocaffcdn.phphub.org/uploads/images/201709/20/1/PtDKbASVcz.png">
                 <div class="card-body">
                     <h5><strong>个人简介</strong></h5>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+                    <p>{{ $user->introduction }}</p>
                     <hr>
                     <h5><strong>注册于</strong></h5>
-                    <p>January 01 1901</p>
+                    <p>{{ $user->created_at->diffForHumans() }}</p>
                 </div>
             </div>
         </div>
@@ -24,8 +24,13 @@
             {{-- 用户发布的内容 --}}
             <div class="card ">
                 <div class="card-body">
-                    暂无数据 ~_~
+                    <h5><strong>个人简介</strong></h5>
+                    <p>{{ $user->introduction }}</p>
+                    <hr>
+                    <h5><strong>注册于</strong></h5>
+                    <p>{{ $user->created_at->diffForHumans() }}</p>
                 </div>
+
             </div>
         </div>
     </div>
